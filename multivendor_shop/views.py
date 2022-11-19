8b8e9537ba4bc6518213abbfd08f4ca1f35a3e30
Md Danish ikbal <88222476+mdcse@users.noreply.github.com>
2022-11-20 01:23:51 +0530

    add-to-cart completed

--- multivendor_shop/views.py
+++ multivendor_shop/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from app.models import Category, Product
 
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login
 from app.models import UserCreateForm
+
+from django.contrib.auth.decorators import login_required
+from cart.cart import Cart
 
 def master(request):
     return redirect('index/')
@@ -50,6 +53,51 @@ def signup(request):
 
     return render(request, 'registration/signup.html', context)
 
-def signin(request):
+def login(request):
     print('sigin called')
     return render(request, 'registration/login.html')
+
+# add to cart
+
+@login_required(login_url="/account/login/")
+def cart_add(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.add(product=product)
+    return redirect("index")
+
+
+@login_required(login_url="/account/login/")
+def item_clear(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.remove(product)
+    return redirect("cart_detail")
+
+
+@login_required(login_url="/account/login/")
+def item_increment(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.add(product=product)
+    return redirect("cart_detail")
+
+
+@login_required(login_url="/account/login/")
+def item_decrement(request, id):
+    cart = Cart(request)
+    product = Product.objects.get(id=id)
+    cart.decrement(product=product)
+    return redirect("cart_detail")
+
+
+@login_required(login_url="/account/login/")
+def cart_clear(request):
+    cart = Cart(request)
+    cart.clear()
+    return redirect("cart_detail")
+
+
+@login_required(login_url="/account/login/")
+def cart_detail(request):
+    return render(request, 'cart/cart_detail.html')
