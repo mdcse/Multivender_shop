9af8c8989e94f9c8be35b646f5889fc3675ec88e
Md Danish ikbal <88222476+mdcse@users.noreply.github.com>
2022-11-22 12:36:15 +0530

    contct_page

--- multivendor_shop/urls.py
+++ multivendor_shop/urls.py
@@ -29,4 +29,13 @@ urlpatterns = [
     path('login', signin, name='login'),
     path('account/', include('django.contrib.auth.urls')),
 
+    #add to cart
+    path('cart/add/<int:id>/', views.cart_add, name='cart_add'),
+    path('cart/item_clear/<int:id>/', views.item_clear, name='item_clear'),
+    path('cart/item_increment/<int:id>/',views.item_increment, name='item_increment'),
+    path('cart/item_decrement/<int:id>/',
+         views.item_decrement, name='item_decrement'),
+    path('cart/cart_clear/', views.cart_clear, name='cart_clear'),
+    path('cart/cart-detail/',views.cart_detail,name='cart_detail'),
+
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
